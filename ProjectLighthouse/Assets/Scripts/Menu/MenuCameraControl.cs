4c8251870adb10fd961270abec8b23d6afeb392a
FilipeMendesMariz <fmendesm@my.centennialcollege.ca>
2024-01-29 14:08:37 -0500

    Added text to main menu and improved the behavior of the menu

--- ProjectLighthouse/Assets/Scripts/Menu/MenuCameraControl.cs
+++ ProjectLighthouse/Assets/Scripts/Menu/MenuCameraControl.cs
@@ -1,106 +1,127 @@
-using Cinemachine;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using static UnityEngine.Rendering.DebugUI;
-
-public class MenuCameraControl : MonoBehaviour
+namespace Menu.MenuCameraControl
 {
-    // Start is called before the first frame update
-    [Header("Cameras")]
-    [SerializeField] CinemachineVirtualCamera mainMenuCamera;
-    [SerializeField] CinemachineVirtualCamera settMenuCamera;
-    [SerializeField] CinemachineVirtualCamera diffMenuCamera;
-    [SerializeField] CinemachineVirtualCamera gameCamera;
+    using Cinemachine;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using static UnityEngine.Rendering.DebugUI;
 
-    [Header("Settings")]
-    [SerializeField] float timeInMsToLift = 20f;
-
-    [Header("Buttons & Images")]
-    [SerializeField] GameObject logo;
-    [SerializeField] Material gameMaterial;
-    [SerializeField] AnimationCurve logoAnimation;
-
-    void Start()
+    public class MenuCameraControl : MonoBehaviour
     {
-        StartCoroutine(StartCamera());
-        StartCoroutine(LogoFadeInFadeOut());
-    }
+        // Start is called before the first frame update
+        [Header("Cameras")]
+        [SerializeField] CinemachineVirtualCamera mainMenuCamera;
+        [SerializeField] CinemachineVirtualCamera settMenuCamera;
+        [SerializeField] CinemachineVirtualCamera diffMenuCamera;
+        [SerializeField] CinemachineVirtualCamera gameCamera;
 
-    IEnumerator StartCamera()
-    {
-        for (int i = 0; i < timeInMsToLift; i++)
+        [Header("Settings")]
+        [SerializeField] float timeInMsToLift = 20f;
+
+        [Header("Buttons & Images")]
+        [SerializeField] GameObject logo;
+        [SerializeField] Material gameMaterial;
+        [SerializeField] AnimationCurve logoAnimation;
+
+        private CameraPositions _currentCameraPosition = CameraPositions.MAIN_MENU;
+
+        void Start()
         {
-            yield return null;
-        }
-        ToMainMenu();
-    }
-
-    IEnumerator LogoFadeInFadeOut()
-    {
-        for (float i = 0; i< timeInMsToLift+1; i++)
-        {
-            float value = logoAnimation.Evaluate(i / timeInMsToLift);
-            logo.GetComponent<Renderer>().material.SetFloat("_Visibility", value);
-            yield return null;
-        }
-    }
-
-    private enum CameraPositions
-    {
-        MAIN_MENU,
-        SETTINGS,
-        DIFFICULTY,
-        GAME
-    }
-
-    private void SetCameraPosition(CameraPositions cameraPosition)
-    {
-        DeactivateAllCameras();
-        switch (cameraPosition)
-        {
-            case CameraPositions.MAIN_MENU:
-                mainMenuCamera.gameObject.SetActive(true);
-                break;
-            case CameraPositions.SETTINGS:
-                settMenuCamera.gameObject.SetActive(true);
-                break;
-            case CameraPositions.DIFFICULTY:
-                diffMenuCamera.gameObject.SetActive(true);
-                break;
-            case CameraPositions.GAME:
-                gameCamera.gameObject.SetActive(true);
-                break;
+            StartCoroutine(StartCamera());
+            StartCoroutine(LogoFadeInFadeOut());
         }
 
+        IEnumerator StartCamera()
+        {
+            for (int i = 0; i < timeInMsToLift; i++)
+            {
+                yield return null;
+            }
+            ToMainMenu();
+        }
+
+        IEnumerator LogoFadeInFadeOut()
+        {
+            for (float i = 0; i < timeInMsToLift + 1; i++)
+            {
+                float value = logoAnimation.Evaluate(i / timeInMsToLift);
+                logo.GetComponent<Renderer>().material.SetFloat("_Visibility", value);
+                yield return null;
+            }
+        }
+
+        private enum CameraPositions
+        {
+            MAIN_MENU,
+            SETTINGS,
+            DIFFICULTY,
+            GAME
+        }
+
+        private void SetCameraPosition(CameraPositions cameraPosition)
+        {
+            DeactivateAllCameras();
+            _currentCameraPosition = cameraPosition;
+            switch (cameraPosition)
+            {
+                case CameraPositions.MAIN_MENU:
+                    mainMenuCamera.gameObject.SetActive(true);
+                    break;
+                case CameraPositions.SETTINGS:
+                    settMenuCamera.gameObject.SetActive(true);
+                    break;
+                case CameraPositions.DIFFICULTY:
+                    diffMenuCamera.gameObject.SetActive(true);
+                    break;
+                case CameraPositions.GAME:
+                    gameCamera.gameObject.SetActive(true);
+                    break;
+            }
+        }
+
+        private void DeactivateAllCameras()
+        {
+            mainMenuCamera.gameObject.SetActive(false);
+            settMenuCamera.gameObject.SetActive(false);
+            diffMenuCamera.gameObject.SetActive(false);
+            gameCamera.gameObject.SetActive(false);
+
+        }
+
+        public void ToSettings()
+        {
+            SetCameraPosition(CameraPositions.SETTINGS);
+        }
+
+        public void ToDifficulty()
+        {
+            SetCameraPosition(CameraPositions.DIFFICULTY);
+        }
+
+        public void ToMainMenu()
+        {
+            SetCameraPosition(CameraPositions.MAIN_MENU);
+        }
+
+        public void ToGame()
+        {
+            SetCameraPosition(CameraPositions.GAME);
+        }
+
+        public void Back()
+        {
+            switch (_currentCameraPosition)
+            {
+                case CameraPositions.MAIN_MENU:
+                    break;
+                case CameraPositions.GAME:
+                    break;
+                case CameraPositions.DIFFICULTY:
+                case CameraPositions.SETTINGS:
+                    ToMainMenu();
+                    break;
+            }
+        }
     }
 
-    private void DeactivateAllCameras()
-    {
-        mainMenuCamera.gameObject.SetActive(false);
-        settMenuCamera.gameObject.SetActive(false);
-        diffMenuCamera.gameObject.SetActive(false);
-        gameCamera.gameObject.SetActive(false);
-
-    }
-
-    public void ToSettings()
-    {
-        SetCameraPosition(CameraPositions.SETTINGS);
-    }
-
-    public void ToDifficulty()
-    {
-        SetCameraPosition(CameraPositions.DIFFICULTY);
-    }
-
-    public void ToMainMenu()
-    {
-        SetCameraPosition(CameraPositions.MAIN_MENU);
-    }
-
-    public void ToGame()
-    {
-        SetCameraPosition(CameraPositions.GAME);
-    }
 }
