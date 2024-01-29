fbb510a1351e4307a6f2b3821e77122fe22a2a49
FilipeMendesMariz <fmendesm@my.centennialcollege.ca>
2024-01-29 14:08:37 -0500

    Added text to main menu and improved the behavior of the menu

--- ProjectLighthouse/Assets/Scripts/Menu/ClickableMenuObject.cs
+++ ProjectLighthouse/Assets/Scripts/Menu/ClickableMenuObject.cs
@@ -1,40 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-
-public class ClickableMenuObject : MonoBehaviour
+namespace Menu.ClickableMenuObjects
 {
-    // Start is called before the first frame update
-    [Header("Function")]
-    [SerializeField] public UnityEvent onClickPerform;
+    using System.Collections;
+    using System.Collections.Generic;
+    using TMPro;
+    using UnityEngine;
+    using UnityEngine.Events;
 
-    private Material _mat;
-    private Vector3 _originalScale;
-    void Start()
+    public class ClickableMenuObject : MonoBehaviour
     {
-        _originalScale = transform.localScale; 
-        _mat = gameObject.GetComponent<Renderer>().material;
-        DisableHighlight();
+        // Start is called before the first frame update
+        [Header("Function")]
+        [SerializeField] public UnityEvent onClickPerform;
+
+        private Material _mat;
+        private Vector3 _originalScale;
+        void Start()
+        {
+            _originalScale = transform.localScale;
+            _mat = gameObject.GetComponent<Renderer>().material;
+            DisableHighlight();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Activate()
+        {
+            onClickPerform.Invoke();
+        }
+        public void Highlight()
+        {
+            _mat.EnableKeyword("GLOW_ON");
+        }
+
+        public void DisableHighlight()
+        {
+            gameObject.transform.localScale = _originalScale;
+            _mat.DisableKeyword("GLOW_ON");
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
-    public void Activate()
-    {
-        onClickPerform.Invoke();
-    }
-    public void Highlight()
-    {
-        gameObject.transform.localScale = _originalScale * 1.4f;
-    }
-
-    public void DisableHighlight()
-    {
-        gameObject.transform.localScale = _originalScale;
-    }
 }
--- ProjectLighthouse/Assets/Scripts/Menu/MenuSelectionController.cs
+++ ProjectLighthouse/Assets/Scripts/Menu/MenuSelectionController.cs
@@ -1,53 +1,83 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MenuSelectionController : MonoBehaviour
+namespace Menu.MenuSelectionController
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    using Menu.ClickableMenuObjects;
+    using Menu.MenuCameraControl;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-    private GameObject _currentMouseOver = null;
-
-    // Update is called once per frame
-    void Update()
+    [RequireComponent(typeof(MenuCameraControl))]
+    public class MenuSelectionController : MonoBehaviour
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        private MenuCameraControl _cameraControl;
+        // Start is called before the first frame update
+        void Start()
         {
-            Debug.Log($"Mouse over {hit.collider.gameObject.name}");
-            if(_currentMouseOver != hit.collider.gameObject)
+            _cameraControl = GetComponent<MenuCameraControl>();
+
+        }
+
+        private GameObject _currentMouseOver = null;
+
+        // Update is called once per frame
+        void Update()
+        {
+            MouseControl();
+            KeyboardControl();
+        }
+
+        private void KeyboardControl()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
-                if(_currentMouseOver != null)
-                {
-                    if (_currentMouseOver.TryGetComponent<ClickableMenuObject>
-                    (out ClickableMenuObject oldhighlightedObj))
-                    {
-                        oldhighlightedObj.DisableHighlight();
-                    }
-                }
-                _currentMouseOver = hit.collider.gameObject;
-                if (hit.transform.gameObject
-                    .TryGetComponent<ClickableMenuObject>
-                    (out ClickableMenuObject highlightedObj))
-                {
-                    highlightedObj.Highlight();
-                }
-            }
-            if (Input.GetMouseButtonDown(0))
-            {
-                if (hit.transform.gameObject
-                    .TryGetComponent<ClickableMenuObject>
-                    (out ClickableMenuObject clickedObject))
-                {
-                    clickedObject.Activate();
-                }
+                _cameraControl.Back();
             }
         }
 
-        
+        private void MouseControl()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Debug.Log($"Mouse over {hit.collider.gameObject.name}");
+                if (_currentMouseOver != hit.collider.gameObject)
+                {
+                    if (_currentMouseOver != null)
+                    {
+                        if (_currentMouseOver.TryGetComponent<ClickableMenuObject>
+                        (out ClickableMenuObject oldhighlightedObj))
+                        {
+                            oldhighlightedObj.DisableHighlight();
+                        }
+                    }
+                    _currentMouseOver = hit.collider.gameObject;
+                    if (hit.transform.gameObject
+                        .TryGetComponent<ClickableMenuObject>
+                        (out ClickableMenuObject highlightedObj))
+                    {
+                        highlightedObj.Highlight();
+                    }
+                }
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (hit.transform.gameObject
+                        .TryGetComponent<ClickableMenuObject>
+                        (out ClickableMenuObject clickedObject))
+                    {
+                        clickedObject.Activate();
+                    }
+                }
+            }
+            else
+            {
+                if (_currentMouseOver.TryGetComponent<ClickableMenuObject>
+                        (out ClickableMenuObject oldhighlightedObj))
+                {
+                    oldhighlightedObj.DisableHighlight();
+                }
+                _currentMouseOver = null;
+            }
+        }
     }
+
 }
